0573d132cdf78c37d6354c01de81179264308875
Garry <vorzel47051@gmail.com>
2023-10-30 20:16:31 +0200

    js-hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -23,6 +23,42 @@ function createGalleryItemsMarkup(items) {
     })
     .join("");
 }
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  const isItemImage = e.target.classList.contains(
+    "gallery__image"
+  );
+  if (!isItemImage) return;
+
+  const currentImgUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+ 		<img src="${currentImgUrl}" width="1280" height="auto"/>
+         `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener(
+          "keydown",
+          onEscKeyPress
+        );
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(e) {
+    const ESC_KEY_CODE = "Escape";
+    const isEscKey = e.code === ESC_KEY_CODE;
+    if (!isEscKey) return;
+    instance.close();
+  }
+}
 
 //варіант 2 (не годиться)
 /*const list = document.querySelector(".gallery");
